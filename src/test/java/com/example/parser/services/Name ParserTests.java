bb8ca7c9d52995ea6de941b5496f59679788ba01
Em <osi-chan@hotmail.fr>
2025-08-04 21:50:33 +0200

    Polished tests

--- src/test/java/com/example/parser/services/Name ParserTests.java
+++ src/test/java/com/example/parser/services/Name ParserTests.java
@@ -11,47 +11,82 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class NameParserTests {
 	private final INameParser nameParser = new NameParser();
 
-	@Test
-	void contextLoads() {
-	}
 
 	@Test
 	@DisplayName("Should parse last-name-first format")
 	void lastNameFirstTest() throws Exception{
-		var result1 = nameParser.parseFullName("Doe, John");
-		var result2 = nameParser.parseFullName("Kristensen, P. H.");
+		//Arrange
+		String case1 = "Doe, John";
+		String case2 = "Kristensen, P. H.";
 
+		//Act
+		var result1 = nameParser.parseFullName(case1);
+		var result2 = nameParser.parseFullName(case2);
+
+		//Assert
 		//To check manually:
-		//System.out.println("First Name: "+ result1.firstName + "\nLast Name: " + result1.lastName);
-		//System.out.println("First Name: "+ result2.firstName + "\nLast Name: " + result2.lastName);
+		//System.out.println("First Name: "+ result1.getFirstName() + "\nLast Name: " + result1.getLastName());
+		//System.out.println("First Name: "+ result2.getFirstName() + "\nLast Name: " + result2.getLastName());
 
-		assertThat(result1.getFirstName()).isEqualTo("John");
-		assertThat(result1.getLastName()).isEqualTo("Doe");
-		assertThat(result2.getFirstName()).isEqualTo("P. H.");
-		assertThat(result2.getLastName()).isEqualTo("Kristensen");
+		assertThat(result1)
+				.isNotNull()
+				.satisfies(a -> {
+					assertThat(result1.getFirstName()).isEqualTo("John");
+					assertThat(result1.getLastName()).isEqualTo("Doe");
+				});
+		assertThat(result2)
+				.isNotNull()
+				.satisfies(a -> {
+					assertThat(result2.getFirstName()).isEqualTo("P. H.");
+					assertThat(result2.getLastName()).isEqualTo("Kristensen");
+				});
 	}
 
 	@Test
-	@DisplayName("Should parse simple first-name-first format")
+	@DisplayName("Should parse first-name-first format")
 	void firstNameFirstTest() throws Exception{
-		var result1 = nameParser.parseFullName("John Doe");
-		var result2 = nameParser.parseFullName("Hans-Christian Jensen");
-		var result3 = nameParser.parseFullName("P. H. Kristensen");
-		var result4 = nameParser.parseFullName("Peter Hans Kristensen");
+		//Arrange
+		String case1 = "John Doe";
+		String case2 = "Hans-Christian Jensen";
+		String case3 = "P. H. Kristensen";
+		String case4 = "Peter Hans Kristensen";
 
+		//Act
+		var result1 = nameParser.parseFullName(case1);
+		var result2 = nameParser.parseFullName(case2);
+		var result3 = nameParser.parseFullName(case3);
+		var result4 = nameParser.parseFullName(case4);
+
+		//Assert
 		//To check manually:
-		//System.out.println("First Name: "+ result1.firstName + "\nLast Name: " + result1.lastName);
-		//System.out.println("First Name: "+ result2.firstName + "\nLast Name: " + result2.lastName);
-		//System.out.println("First Name: "+ result3.firstName + "\nLast Name: " + result3.lastName);
-		//System.out.println("First Name: "+ result4.firstName + "\nLast Name: " + result4.lastName);
+		//System.out.println("First Name: "+ result1.getFirstName() + "\nLast Name: " + result1.getLastName());
+		//System.out.println("First Name: "+ result2.getFirstName() + "\nLast Name: " + result2.getLastName());
+		//System.out.println("First Name: "+ result3.getFirstName() + "\nLast Name: " + result3.getLastName());
+		//System.out.println("First Name: "+ result4.getFirstName() + "\nLast Name: " + result4.getLastName());
 
-		assertThat(result1.getFirstName()).isEqualTo("John");
-		assertThat(result1.getLastName()).isEqualTo("Doe");
-		assertThat(result2.getFirstName()).isEqualTo("Hans-Christian");
-		assertThat(result2.getLastName()).isEqualTo("Jensen");
-		assertThat(result3.getFirstName()).isEqualTo("P. H.");
-		assertThat(result3.getLastName()).isEqualTo("Kristensen");
-		assertThat(result4.getFirstName()).isEqualTo("Peter Hans");
-		assertThat(result4.getLastName()).isEqualTo("Kristensen");
+		assertThat(result1)
+				.isNotNull()
+				.satisfies(a -> {
+					assertThat(result1.getFirstName()).isEqualTo("John");
+					assertThat(result1.getLastName()).isEqualTo("Doe");
+				});
+		assertThat(result2)
+				.isNotNull()
+				.satisfies(a -> {
+					assertThat(result2.getFirstName()).isEqualTo("Hans-Christian");
+					assertThat(result2.getLastName()).isEqualTo("Jensen");
+				});
+		assertThat(result3)
+				.isNotNull()
+				.satisfies(a -> {
+					assertThat(result3.getFirstName()).isEqualTo("P. H.");
+					assertThat(result3.getLastName()).isEqualTo("Kristensen");
+				});
+		assertThat(result4)
+				.isNotNull()
+				.satisfies(a -> {
+					assertThat(result4.getFirstName()).isEqualTo("Peter Hans");
+					assertThat(result4.getLastName()).isEqualTo("Kristensen");
+				});
 	}
 }
